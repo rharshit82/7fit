0f3765d044bc2f9feb725f7c06fa08eb8ae0c896
utkarshsingh20520 <utkarsh2052000@gmail.com>
2021-11-14 01:53:07 +0530

    Landing page done

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './Banner.css'
+import React from "react";
+import "./Banner.css";
 
 function Banner() {
-    return (
-        <div className="banner">
-            <div className="bannersec">
-                <div className="headingban">
-                    <h1>7Fit</h1>
-                    <p>Fitness hack for HackFit</p>
-                </div>
-            </div>
+  return (
+    <div className="banner">
+      <div className="bannersec">
+        <div className="headingban">
+          <div>
+            <h1>7Fit</h1>
+            <p>Fitness hack for HackFit</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ client/src/components/Services/Service.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Service.css";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Service() {
+  return (
+    <div className="my-5 Service container">
+      <div className="Serviceban">
+        <h1>Unique Feature</h1>
+
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-lg-5 col-sm-12 card p-0">
+              <LinkContainer to="/upload">
+                <div className="cardname">
+                  <h1>Upload</h1>
+                  <p>
+                    The following feature will help you to upload your
+                    accomplishment on the main wall through which you can
+                    showcase your accomplishment.
+                  </p>
+                </div>
+              </LinkContainer>
+            </div>
+
+            <div className="col-lg-2 col-sm-0"></div>
+
+            <LinkContainer to="/wall">
+              <div className="col-lg-5 col-sm-12 card p-0">
+                <div className="cardname">
+                  <h1>FitWall</h1>
+                  <p>
+                    In the following section you can easily watch your post as
+                    well as other post and through this page you can upvote
+                    their post.
+                  </p>
+                </div>
+              </div>
+            </LinkContainer>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
